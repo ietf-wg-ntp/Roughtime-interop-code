da4caaa1822e9f45a3f4ef22a883ee0f55a16b7b
Sarah Grant <sarah.grant.ietf@gmail.com>
2024-10-31 16:16:32 +0000

    Further work on the client

--- plummet/cli.py
+++ plummet/cli.py
@@ -1,6 +1,8 @@
 import sys
+import os
 import yaml
 import logging
+import datetime
 import argparse
 from argparse import ArgumentParser
 from typing import Dict, List
@@ -10,30 +12,57 @@ plummet takes all the known roughtime implementations and attempts to perform
 interop tests against each client/server pair.
 """
 
-# Load the JSON file with the list of servers and clients
-# Make a list of permutations
-# Run Docker
-
-# TODO: How to get packet captures
-# TODO: Work out output directories
-# TODO: Packet Capture
-# TODO: Work out serving results - HTML?
 
 def main() -> None:
-    logger = logging.getLogger('plummet')
-
     parser = ArgumentParser(description=CMD_INFO)
     parser.add_argument('--impls', type=argparse.FileType('r'),
                         default='implementations/implementations.yml',
                         help='Path to the implementations YAML file')
+    parser.add_argument('--output-dir', default='results/',
+                        help='Base directory to write data out to')
     parser.add_argument('--dry-run', action=argparse.BooleanOptionalAction,
                         help="Don't make any system calls, just print")
+    parser.add_argument('--runner', default='docker',
+                        help='Path to a different container runtime')
+    parser.add_argument('--verbose', action=argparse.BooleanOptionalAction,
+                        help='Enable more verbosity')
     args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
+    else:
+        logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger('plummet')
+
     try:
         args.impls = yaml.safe_load(args.impls.read())
     except yaml.YAMLError as e:
         logger.critical('Implementations YAML appaers bad, exiting...')
         sys.exit(1)
+
+    # Generate all the permutations of client and servers
+    permutations = generate_permutations(args.impls['implementations'])
+
+    # Prepare the location
+    start_time = datetime.datetime.now(datetime.UTC)
+    # Colons and other characters can be iffy in folder names
+    results_dir = start_time.strftime("%Y%m%dT%H%M%SZ")
+
+    if args.dry_run:
+        logger.info('Dry-run: Create folder {results_dir}')
+    else:
+        # os.mkdir()
+
+        pass
+
+    # For each generate the docker command to run for them
+    for perm in permutations:
+        cmd = generate_cmd(perm)
+
+
+# Generate the docker command to run for a given permutation
+def generate_cmd(perm: Dict) -> str:
+    return ""
 
 
 # For each implementation that we know of, based on it having a client and/or
