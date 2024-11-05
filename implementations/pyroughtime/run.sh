b704810fcd03b9b7b29c9cd016cfe93f36abe588
Marcus Dansarie <marcus@dansarie.se>
2024-11-05 18:58:35 +0000

    Pyroughtime server support.

--- implementations/pyroughtime/run.sh
+++ implementations/pyroughtime/run.sh
@@ -3,10 +3,12 @@
 MODE=$1
 
 PUB_KEY='Ixu7gqjJ9TU6IxsO8wxZxAFT5te6FcZZQq5vXFl35JE='
+PRIV_KEY='BuXi3Chpe7Nj3gCXavLUIoGbxngyrWVa3pYIHswbzbU='
 
 if [[ "$MODE" == "client" ]]; then
     /usr/src/app/pyroughtime.py -s roughtime-server 2002 $PUB_KEY &> /data/client.log;
 elif [[ "$MODE" == "server" ]]; then
+    /usr/src/app/pyroughtime.py -t $PRIV_KEY &> /data/server.log;
     echo "Not supported?";
     exit 1;
 else
