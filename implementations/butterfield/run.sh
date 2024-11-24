9fd3883c585c057eab5e5fd2a7615c8903017c27
Sarah Grant <sarah.grant.ietf@gmail.com>
2024-11-24 12:42:27 +0000

    Butterfield should run, better.

--- implementations/butterfield/run.sh
+++ implementations/butterfield/run.sh
@@ -6,7 +6,7 @@ if [[ "$MODE" == "client" ]]; then
     echo "Client mode not supported!";
     exit 1;
 elif [[ "$MODE" == "server" ]]; then
-    cd /usr/src/app && /usr/local/bin/iex -S mix &> /data/server.log;
+    cd /usr/src/app && /usr/local/bin/mix run --no-halt &> /data/server.log;
 else
     echo "No mode specified, exiting!";
     exit 1;
