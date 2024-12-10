c09e3fb405a8e5d970e8bf59805e7dcca7432664
Marcus Dansarie <marcus@dansarie.se>
2024-12-10 13:01:47 +0100

    Fix Butterfield run file.

--- implementations/butterfield/run.sh
+++ implementations/butterfield/run.sh
@@ -6,7 +6,7 @@ if [[ "$MODE" == "client" ]]; then
     echo "Client mode not supported!";
     exit 1;
 elif [[ "$MODE" == "server" ]]; then
-    MIX_ENV = "dev" cd /usr/src/app && /usr/local/bin/mix run --no-halt &> /data/server.log;
+    MIX_ENV="dev" cd /usr/src/app && /usr/local/bin/mix run --no-halt &> /data/server.log;
 else
     echo "No mode specified, exiting!";
     exit 1;
