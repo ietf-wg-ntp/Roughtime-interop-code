0ccb4a726db67e79004ab03cf1c0086760d26f37
teaishealthy <76410798+teaishealthy@users.noreply.github.com>
2026-01-27 00:52:55 +0000

    feat: add roughly support

--- implementations/roughly/run.sh
+++ implementations/roughly/run.sh
@@ -4,9 +4,9 @@ MODE=$1
 
 if [[ "$MODE" == "client" ]]; then
     sleep 2;
-	python3 /usr/src/app/run.py &> /data/client.log;
+	roughly -v query roughtime-server 2002 Ixu7gqjJ9TU6IxsO8wxZxAFT5te6FcZZQq5vXFl35JE= &> /data/client.log
 elif [[ "$MODE" == "server" ]]; then
-    echo "Not implemented";
+    ROUGHLY_PRIVATE_KEY=BuXi3Chpe7Nj3gCXavLUIoGbxngyrWVa3pYIHswbzbU= roughly -v server run &> /data/server.log
 	exit 1;
 else
     echo "No mode specified, exiting!";
