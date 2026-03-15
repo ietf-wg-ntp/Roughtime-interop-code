f0f3d74f0d27692a7f9c5547d713ea0271e3b104
Marcus Dansarie <marcus@dansarie.se>
2026-03-15 22:04:17 +0100

    Update pyroughtime run script for new command line argument.

--- implementations/pyroughtime/run.sh
+++ implementations/pyroughtime/run.sh
@@ -8,7 +8,7 @@ PRIV_KEY='BuXi3Chpe7Nj3gCXavLUIoGbxngyrWVa3pYIHswbzbU='
 if [[ "$MODE" == "client" ]]; then
     sleep 2 && /usr/src/app/pyroughtime.py -s roughtime-server 2002 $PUB_KEY &> /data/client.log;
 elif [[ "$MODE" == "server" ]]; then
-    /usr/src/app/pyroughtime.py -t $PRIV_KEY &> /data/server.log;
+    /usr/src/app/pyroughtime.py -t $PRIV_KEY 2002 &> /data/server.log;
 else
     echo "No mode specified, exiting!";
     exit 1;
